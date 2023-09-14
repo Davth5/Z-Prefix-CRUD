39b1c098f2d9164f3571a01495928a175c0f55d1
Davth5 <davth5@gmail.com>
2023-09-13 21:22:09 -0600

    Updated more seeds and hashed passwords in 01_users.js seeds so they work

--- server/seeds/01_users.js
+++ server/seeds/01_users.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -5,19 +7,43 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
+
+  const johnPassword = await bcrypt.hash("12345", 10);
+  const janePassword = await bcrypt.hash("4321", 10);
+  const alicePassword = await bcrypt.hash("alice123", 10);
+  const bobPassword = await bcrypt.hash("bob123", 10);
+  const charliePassword = await bcrypt.hash("charlie123", 10);
+
   await knex("users").insert([
     {
       firstName: "John",
       lastName: "Doe",
       userName: "john.doe",
-      password: "12345",
+      password: johnPassword,
     },
     {
-      id: 2,
       firstName: "Jane",
       lastName: "Doe",
       userName: "jane.doe",
-      password: "4321",
+      password: janePassword,
+    },
+    {
+      firstName: "Alice",
+      lastName: "Smith",
+      userName: "alice.smith",
+      password: alicePassword,
+    },
+    {
+      firstName: "Bob",
+      lastName: "Johnson",
+      userName: "bob.johnson",
+      password: bobPassword,
+    },
+    {
+      firstName: "Charlie",
+      lastName: "Brown",
+      userName: "charlie.brown",
+      password: charliePassword,
     },
   ]);
 };
--- server/seeds/02_items.js
+++ server/seeds/02_items.js
@@ -9,13 +9,40 @@ exports.seed = async function (knex) {
     {
       userId: 1,
       itemName: "Laptop",
-      description: "Macbook Pro",
+      description: "Macbook Pro 16-inch with M1 chip, 32GB RAM, 1TB SSD",
       quantity: 1,
     },
     {
       userId: 2,
       itemName: "Phone",
-      description: "iPhone 12",
+      description: "iPhone 13 Pro Max with 256GB storage, Graphite color",
+      quantity: 1,
+    },
+    {
+      userId: 1,
+      itemName: "Headphones",
+      description:
+        "Sony WH-1000XM4 Wireless Noise-Canceling Over-Ear Headphones",
+      quantity: 2,
+    },
+    {
+      userId: 2,
+      itemName: "Camera",
+      description: "Canon EOS R6 Full-Frame Mirrorless Camera with 4K Video",
+      quantity: 1,
+    },
+    {
+      userId: 1,
+      itemName: "Smartwatch",
+      description:
+        "Apple Watch Series 7, 45mm, GPS + Cellular, Midnight Aluminum Case",
+      quantity: 1,
+    },
+    {
+      userId: 2,
+      itemName: "Tablet",
+      description:
+        "Samsung Galaxy Tab S7+ with 12.4-inch display, 128GB storage, Mystic Black",
       quantity: 1,
     },
   ]);
