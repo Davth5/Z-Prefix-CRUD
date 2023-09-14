5a86d88f0695e7b0d8e71f82e26c72b50870d814
Davth5 <davth5@gmail.com>
2023-09-13 23:27:04 -0600

    Given up on docker compose for whole app for now

--- server/routes/app.js
+++ server/routes/app.js
@@ -4,8 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
-
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 const usersRoutes = require("./users");
